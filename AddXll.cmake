a3a4fac55b5ad55d7b0bcca28aea2bd34fb38690
quintron <quintron@users.noreply.github.com>
2019-03-24 13:30:27 +0100

    format formatting

--- AddXll.cmake
+++ AddXll.cmake
@@ -4,31 +4,31 @@ message(STATUS "XLW_PATH=${XLW_PATH}")
 macro(add_xll xllname api_file_name)
     message(STATUS "Add Xll: ${xllname}")
 	
-	message(STATUS "create wrapper xlw${api_file_name}.cpp")
-	file(WRITE xlw${api_file_name}.cpp)
+    message(STATUS "create wrapper xlw${api_file_name}.cpp")
+    file(WRITE xlw${api_file_name}.cpp)
+
+    set(file_list "${ARGN}")
+    list(APPEND file_list
+	 ${api_file_name}.h
+         ${api_file_name}.cpp
+         xlw${api_file_name}.cpp
+    )
 	
-	set(file_list "${ARGN}")
-	list(APPEND file_list
-	    ${api_file_name}.h
-		${api_file_name}.cpp
-		xlw${api_file_name}.cpp
-		)
-	
-	add_library(${xllname} SHARED ${file_list})
-	set_target_properties(${xllname} PROPERTIES SUFFIX ".xll")
-	target_include_directories(${xllname} PUBLIC
+    add_library(${xllname} SHARED ${file_list})
+    set_target_properties(${xllname} PROPERTIES SUFFIX ".xll")
+    target_include_directories(${xllname} PUBLIC
         $<BUILD_INTERFACE:${XLW_PATH}/xlw/include>
         $<INSTALL_INTERFACE:${XLW_PATH}/xlw/include>		
-	)
-	target_link_libraries(${xllname}
-	    "$<$<CONFIG:Debug>:${XLW_PATH}/xlw/lib/xlw-vc140-mt-gd.lib>"
-		"$<$<NOT:$<CONFIG:Debug>>:${XLW_PATH}/xlw/lib/xlw-vc140-mt.lib>"
-	)
+    )
+    target_link_libraries(${xllname}
+        "$<$<CONFIG:Debug>:${XLW_PATH}/xlw/lib/xlw-vc140-mt-gd.lib>"
+        "$<$<NOT:$<CONFIG:Debug>>:${XLW_PATH}/xlw/lib/xlw-vc140-mt.lib>"
+    )
     
     add_custom_command(TARGET ${xllname}
                        PRE_BUILD
-					   COMMAND ${XLW_PATH}/xlw/InterfaceGenerator.exe ${CMAKE_CURRENT_SOURCE_DIR}/${api_file_name}.h
-					   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                       COMMAND ${XLW_PATH}/xlw/InterfaceGenerator.exe ${CMAKE_CURRENT_SOURCE_DIR}/${api_file_name}.h
+                       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
     )					   
-	message(STATUS "Add Xll done")
+    message(STATUS "Add Xll done")
 endmacro()
